398a47c151dd68d401e2089c72aa7a8c405059fd
phongnguyend <nguyendoanphong.it@gmail.com>
2019-04-08 15:01:55 +0700

    Show Icon, Platform, Description

--- src/HeapInspector/HeapInspector/Components/SelectProcess.Designer.cs
+++ src/HeapInspector/HeapInspector/Components/SelectProcess.Designer.cs
@@ -28,9 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.processesGrid = new System.Windows.Forms.DataGridView();
+            this.clIcon = new System.Windows.Forms.DataGridViewImageColumn();
             this.hdName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.hdPID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Platform = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.processesGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,8 +47,11 @@
             this.processesGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.processesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.processesGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clIcon,
             this.hdName,
-            this.hdPID});
+            this.hdPID,
+            this.Platform,
+            this.Description});
             this.processesGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.processesGrid.Location = new System.Drawing.Point(0, 0);
             this.processesGrid.MultiSelect = false;
@@ -54,15 +62,49 @@
             this.processesGrid.TabIndex = 0;
             this.processesGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ProcessesGrid_CellDoubleClick);
             // 
+            // clIcon
+            // 
+            this.clIcon.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.clIcon.FillWeight = 10F;
+            this.clIcon.HeaderText = "";
+            this.clIcon.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.clIcon.Name = "clIcon";
+            this.clIcon.ReadOnly = true;
+            this.clIcon.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.clIcon.Width = 18;
+            // 
             // hdName
             // 
+            this.hdName.FillWeight = 105.7894F;
             this.hdName.HeaderText = "Name";
             this.hdName.Name = "hdName";
             // 
             // hdPID
             // 
+            this.hdPID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.hdPID.DefaultCellStyle = dataGridViewCellStyle1;
+            this.hdPID.FillWeight = 105.7894F;
             this.hdPID.HeaderText = "PID";
             this.hdPID.Name = "hdPID";
+            this.hdPID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Platform
+            // 
+            this.Platform.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Platform.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Platform.FillWeight = 105.7894F;
+            this.Platform.HeaderText = "Platform";
+            this.Platform.Name = "Platform";
+            this.Platform.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Description
+            // 
+            this.Description.FillWeight = 105.7894F;
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            this.Description.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // SelectProcess
             // 
@@ -82,7 +124,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView processesGrid;
+        private System.Windows.Forms.DataGridViewImageColumn clIcon;
         private System.Windows.Forms.DataGridViewTextBoxColumn hdName;
         private System.Windows.Forms.DataGridViewTextBoxColumn hdPID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Platform;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
     }
 }
--- src/HeapInspector/HeapInspector/Components/SelectProcess.cs
+++ src/HeapInspector/HeapInspector/Components/SelectProcess.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,11 +14,18 @@ namespace HeapInspector.Components
 {
     public partial class SelectProcess : Form
     {
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool IsWow64Process([In] IntPtr process, [Out] out bool wow64Process);
+
         public int SelectedProcess { get; private set; }
 
         public SelectProcess()
         {
             InitializeComponent();
+            Text += $" ({GetPlatform(Process.GetCurrentProcess())})";
+            processesGrid.Columns["hdPID"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.BottomRight;
+            processesGrid.Columns["Platform"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.BottomRight;
         }
 
         private void SelectProcess_Load(object sender, EventArgs e)
@@ -35,13 +43,27 @@ namespace HeapInspector.Components
                 }
                 rowcount++;
 
+                DataGridViewImageCell icon = new DataGridViewImageCell();
+                icon.Value = GetIcon(process);
+                gridrow.Cells.Add(icon);
+
                 DataGridViewTextBoxCell name = new DataGridViewTextBoxCell();
                 name.Value = process.ProcessName;
                 gridrow.Cells.Add(name);
 
                 DataGridViewTextBoxCell pid = new DataGridViewTextBoxCell();
                 pid.Value = process.Id;
+                pid.Style.Alignment = DataGridViewContentAlignment.BottomRight;
                 gridrow.Cells.Add(pid);
+
+                DataGridViewTextBoxCell platform = new DataGridViewTextBoxCell();
+                platform.Value = GetPlatform(process);
+                platform.Style.Alignment = DataGridViewContentAlignment.BottomRight;
+                gridrow.Cells.Add(platform);
+
+                DataGridViewTextBoxCell description = new DataGridViewTextBoxCell();
+                description.Value = GetDescription(process);
+                gridrow.Cells.Add(description);
 
                 processesGrid.Rows.Add(gridrow);
             }
@@ -51,8 +73,63 @@ namespace HeapInspector.Components
         {
             if (e.RowIndex >= 0)
             {
-                SelectedProcess = Convert.ToInt32(processesGrid.Rows[e.RowIndex].Cells[1].Value.ToString());
+                SelectedProcess = Convert.ToInt32(processesGrid.Rows[e.RowIndex].Cells[2].Value.ToString());
                 Close();
+            }
+        }
+
+        private static string GetPlatform(Process process)
+        {
+            if (!Environment.Is64BitOperatingSystem)
+                return "32 bit";
+
+            try
+            {
+                if ((Environment.OSVersion.Version.Major > 5)
+                || ((Environment.OSVersion.Version.Major == 5) && (Environment.OSVersion.Version.Minor >= 1)))
+                {
+                    bool retVal;
+
+                    return IsWow64Process(process.Handle, out retVal) && retVal ? "32 bit" : "64 bit";
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                if (ex.NativeErrorCode != 0x00000005)
+                {
+                    throw;
+                }
+            }
+
+            return "";
+        }
+
+        private static string GetDescription(Process process)
+        {
+            try
+            {
+                var desc = process.MainModule.FileVersionInfo.FileDescription;
+                if (string.IsNullOrEmpty(desc))
+                    return process.MainModule.FileVersionInfo.FileName;
+                return desc;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private static object GetIcon(Process process)
+        {
+            try
+            {
+                var icon = Icon.ExtractAssociatedIcon(process.MainModule.FileName);
+                return new Bitmap(icon.ToBitmap(), 16, 16);
+            }
+            catch (Exception)
+            {
+
+                return null;
             }
         }
     }
